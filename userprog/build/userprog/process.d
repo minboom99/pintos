19ec86ea146740fd8f25350fb2218b3aaf5aa535
Minbeom Kim <alsqja1754@gmail.com>
2021-04-13 22:35:15 +0900

    going crazy

--- userprog/build/userprog/process.d
+++ userprog/build/userprog/process.d
@@ -15,4 +15,4 @@ userprog/process.o: ../../userprog/process.c \
  ../../include/threads/init.h ../../include/threads/palloc.h \
  ../../include/threads/mmu.h ../../include/threads/pte.h \
  ../../include/threads/vaddr.h ../../include/intrinsic.h \
- ../../include/threads/mmu.h ../../include/threads/malloc.h
+ ../../include/threads/mmu.h
